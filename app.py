6c91faea0cda851b2b9fdf04fed32f7d5995e418
ozseniorcl13 <seniorcl13@gmail.com>
2022-11-14 12:27:20 -0400

    feat: add section upload dataset

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 import streamlit as st
+import pandas as pd
 
 with st.sidebar:
     st.title('Auto ML App')
-    choice = st.radio('Navigation', ['Upload', 'Data Analysis', 'Train model', 'Download model'])
+    choice = st.radio('Navigation', ['Upload dataset', 'Explore and Analysis your data', 'Train moodel', 'Get model'])
     st.info('Load your dataset, explore your data and train model automated')
+    
+if choice == 'Upload dataset':
+    st.title('Upload your data for AutoML')
+    file = st.file_uploader("Upload your dataset here")
+    if file:
+        df = pd.read_csv(file, index_col = None)
+        df.to_csv("source_data.csv", index = None)
+        st.dataframe(df)
